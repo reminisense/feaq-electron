d4ade937bf989c58a65e2730df9e873b0e2587aa
Aunne Arzadon <nodazra@gmail.com>
2016-05-20 15:50:36 +0800

    ARA Updated Terminals layout ARA 05202016 Improved UI for terminal selection

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const development = false;
 
 
 global.urls = {
-    app_url: development ? 'http://localhost:8000' : 'http://four.featherq.com',
+    app_url: development || true? 'http://localhost:8000' : 'http://four.featherq.com',
     websocket_url: "ws://188.166.234.33:443/socket/server.php"
 };
 
